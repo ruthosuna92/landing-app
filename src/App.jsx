cca3b54aa858781030457cd3b9dbf0b50b9c711c
alejandra <alejandra@emotecare.com>
2025-05-07 19:22:37 +0100

    chore: integrate Tailwind CSS and update styles
    - Imported Tailwind CSS in index.css and removed previous global styles.
    - Updated font family to 'Monserrat' in the body.
    - Modified vite.config.js to include Tailwind CSS plugin.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import RickAndMorty from './assets/rick-and-morty-logo.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className='flex flex-col items-center justify-center h-[100dvh] gap-10 lg:gap-12 font-[Montserrat]'>
+        <img className='lg:w-[500px]' src={RickAndMorty} alt="Rick and Morty" />
+        <div className='text-white text-center px-4 flex gap-4 flex-col lg:w-[872px]'>
+          <h1 className='text-2xl lg:text-[2.25rem] font-bold'>
+            Bienvenido a Rick and Morty
+          </h1>
+          <p className='text-sm lg:text-lg font-medium lg:font-semibold'>
+            En esta prueba, evaluaremos su capacidad para construir la aplicación mediante el análisis de código y la reproducción del siguiente diseño.
+          </p>
+        </div>
+        <a className='bg-[#8BC547] h-[2.75rem] flex px-[1.375rem] items-center rounded-full text-sm font-bold' href="#"><span className='text-[#354E18]'>
+          Comenzar
+          </span>
+          </a>
+      </section>
     </>
   )
 }
